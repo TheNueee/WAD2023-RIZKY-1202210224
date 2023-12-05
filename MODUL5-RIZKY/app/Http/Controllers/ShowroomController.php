4b3b04424947fb388f6b98e8adde433c57d3ee99
TheNueee <rizkyalhakim1@gmail.com>
2023-12-05 11:25:46 +0700

    modul5

---
+++ MODUL5-RIZKY/app/Http/Controllers/ShowroomController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ShowroomController extends Controller
+{
+
+public function create(){
+    return view();
+}
+
+public function store(){
+
+}
+
+public function index(){
+    // $mobil = Mobil:all();
+    return view();
+}
+
+}
